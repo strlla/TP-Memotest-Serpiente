d871467dfb54a2010d9b07375803964f0fc7ad0f
JTejad <93409444+JTejad@users.noreply.github.com>
2021-11-05 21:17:38 -0300

    complistas-ocultar-fichas
    Modifique los comentarios de las funciones y añadí comprensión de listas en la función ocultar-fichas

--- Etapa 1.2.py
+++ Etapa 1.2.py
@@ -1,37 +1,41 @@
 from os import system
 
+
 def generador_fichas():
+    """Genera las fichas principales para dar inicio al juego"""
     # shuffle(fichas) Esto serviria para la aleatoriedad mas adelante
 
-    fichas = ["D", "D", "s", "s"]  # FICHAS DADAS POR ENUNCIADO
+    fichas = ["D", "D", "s", "s"]
 
     return fichas
 
-def revisar_fichas(fichas, fichas_ocultas): 
-    #funcion que revisa si todas las fichas estan descubiertas (si el jugador gano) y retorna True, en caso contrario retorna False
-    # Estrella Portocarrero 
+
+def revisar_fichas(fichas, fichas_ocultas):
+    """Funcion que revisa si todas las fichas estan descubiertas (si el jugador gano) y retorna True, en caso contrario retorna False
+    # Estrella Portocarrero """
+
     return all(ficha == ficha_oculta[1] for ficha, ficha_oculta in zip(fichas, fichas_ocultas))
 
-def ocultar_fichas(fichas):
-    # Se itera segun la longitud de las fichas dadas para ocultarlas como numeros
-    fichas_ocultas = []
 
-    for numeros in range(len(fichas)):  
-        fichas_ocultas.append(str([numeros]))
+def ocultar_fichas(fichas):
+    """ Se itera segun la longitud de las fichas dadas para ocultarlas como numeros"""
+
+    fichas_ocultas = [(str([numeros])) for numeros in range(len(fichas))]
 
     return fichas_ocultas
 
+
 def jugada(fichas, fichas_ocultas):
-    #funcion principal que permite al jugador seguir jugando mientras encuentre pares iguales, caso contrario se resetean las fichas.
-    #Estrella Portocarrero
-    
+    """Funcion principal que permite al jugador seguir jugando mientras encuentre pares iguales, caso contrario se resetean las fichas.
+    Estrella Portocarrero"""
+
     print("Fichas y posiciones:", *fichas_ocultas)
     gano = False
     while not gano:
         resultado, fichas_ocultas = seleccionar_posiciones(fichas, fichas_ocultas)
         if resultado:
             gano = revisar_fichas(fichas, fichas_ocultas)
-        else:        
+        else:
             system("cls")
 
             print("Intenta nuevamente")
@@ -40,20 +44,21 @@ def jugada(fichas, fichas_ocultas):
 
             fichas_ocultas = ocultar_fichas(fichas)
 
-            print("Fichas y posiciones:", *fichas_ocultas)        
+            print("Fichas y posiciones:", *fichas_ocultas)
 
-    print("Ganaste!")
-    
+    print(f"¡Felicitaciones!")
+
+
 def seleccionar_posiciones(fichas, fichas_ocultas):
-    # funcion que permite el ingreso de las posiciones a descubrir en las fichas y retorna True en caso de que sean iguales, en caso contrario retorna False
-    
+    """Funcion que permite el ingreso de las posiciones a descubrir en las fichas y retorna True en caso de que sean iguales, en caso contrario retorna False"""
+
     primer_posicion = input("1er posición: ")
     primer_es_valido = validar_ingreso(primer_posicion, fichas_ocultas)
     while not primer_es_valido:
-      primer_posicion = input("1er posición: ")
-      primer_es_valido = validar_ingreso(primer_posicion, fichas_ocultas)
+        primer_posicion = input("1er posición: ")
+        primer_es_valido = validar_ingreso(primer_posicion, fichas_ocultas)
     primer_posicion = int(primer_posicion)
-    
+
     fichas_ocultas = mostrar_ficha(primer_posicion, fichas, fichas_ocultas)
 
     segunda_posicion = input("2da posición: ")
@@ -71,32 +76,35 @@ def seleccionar_posiciones(fichas, fichas_ocultas):
 
 
 def comparar_fichas(primera_posicion, segunda_posicion, fichas):
-    # compara el contenido de la ficha en las dos posiciones recibidas, retorna True si son iguales, en caso contrario retorna False
+    """Compara el contenido de la ficha en las dos posiciones recibidas, retorna True si son iguales, en caso contrario retorna False"""
     return fichas[primera_posicion] == fichas[segunda_posicion]
 
+
 def mostrar_ficha(posicion, fichas, fichas_ocultas):
-    # printea las fichas ocultas y las descubiertas por el usuario
+    """Printea las fichas ocultas y las descubiertas por el usuario"""
     fichas_ocultas = [i.replace(str(posicion), fichas[posicion]) for i in fichas_ocultas]
 
     print("Fichas y posiciones:", *fichas_ocultas)
 
     return fichas_ocultas
 
-def validar_ingreso(posicion, fichas_ocultas):    
-    #Funcion que chequea que la posicion ingresada sea numerica  y sea menor o igual a la cantidad de fichas, falta validar que este disponible
-    #Estrella Portocarrero
+
+def validar_ingreso(posicion, fichas_ocultas):
+    """Funcion que chequea que la posicion ingresada sea numerica  y sea menor o igual a la cantidad de fichas, falta validar que este disponible
+    Estrella Portocarrero"""
     if not posicion.isnumeric():
         print("Ingrese un valor numerico")
         return False
-    else: 
+    else:
         if int(posicion) > len(fichas_ocultas) - 1 or int(posicion) < 0:
-            print("Ingrese un numero menor o igual a ",len(fichas_ocultas) - 1)
+            print("Ingrese un numero menor o igual a ", len(fichas_ocultas) - 1)
             return False
         elif not fichas_ocultas[int(posicion)][1].isnumeric():
             print("Esta ficha ({}) no esta disponible".format(fichas_ocultas[int(posicion)][1]))
             return False
-            
+
     return True
+
 
 def main():
     fichas = generador_fichas()
