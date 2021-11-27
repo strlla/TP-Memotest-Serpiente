b8feb8b859ae8074af6b1f9bed245410dec46ce9
Estrella Portocarrero <estrellaportocarrero@gmail.com>
2021-11-27 13:30:09 -0300

    Se crea una clase Interfaz

--- login_registro.py
+++ login_registro.py
@@ -1,3 +1,4 @@
+
 import csv
 
 def guardar_nuevo_usuario(usuario):    
@@ -18,11 +19,11 @@ def obtener_usuarios():
     return usuarios[1:len(usuarios)]        
     
 
-def iniciar_sesion(usuario, contrasenia):
+def iniciar_sesion(mostrar_mensaje_error, usuario, contrasenia):
     usuarios = obtener_usuarios()
     usuarioEncontrado = next((x for x in usuarios if x["usuario"] == usuario), None)
     if (not usuarioEncontrado): 
-        print("el usuario no existe")
+        mostrar_mensaje_error("No estas registrado!")
     else: 
         if (usuarioEncontrado['clave'] == contrasenia):
             print("logueado!") 
