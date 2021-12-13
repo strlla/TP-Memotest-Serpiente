c386b780e2f5f5a8776f4997ae5a3e8703199f94
Juan <93409444+JTejad@users.noreply.github.com>
2021-12-13 17:58:01 -0300

    Update partida

--- partida.py
+++ partida.py
@@ -1,9 +1,13 @@
 from tkinter import *
 import tkinter as tk
 from tkinter import messagebox
-from juego import Juego
+
 
 class Partida:
+
+    def __init__(self) -> None:
+        self.partidas = []
+        self.resumen = []
 
     def guardar_hora_finalizacion_partida(self, hora_finalizacion):
         self.hora_finalizacion = hora_finalizacion
@@ -12,13 +16,11 @@ class Partida:
         self.fecha_partida = fecha_partida
 
     def generar_resumen_partida(self):
-        partidas = Juego.partidas
-        resumen = Juego.resumen
-        nombres = set(list(partidas[0].keys()))
+        nombres = set(list(self.partidas[0].keys()))
 
         for nombre in nombres:
-            aciertos = sum([partida[nombre]['aciertos'] for partida in partidas])
-            intentos = sum([partida[nombre]['intentos'] for partida in partidas])
+            aciertos = sum([partida[nombre]['aciertos'] for partida in self.partidas])
+            intentos = sum([partida[nombre]['intentos'] for partida in self.partidas])
             resumen_partida = {
                 "fecha_partida": self.fecha_partida,
                 "hora_finalizacion": self.hora_finalizacion,
@@ -26,18 +28,20 @@ class Partida:
                 "aciertos": aciertos,
                 "intentos": intentos
             }
-            resumen.append(resumen_partida)
+            self.resumen.append(resumen_partida)
 
-        Juego.resumen = sorted(resumen, key=lambda x: x['aciertos'], reverse=True)
+        self.resumen = sorted(self.resumen, key=lambda x: x['aciertos'], reverse=True)
+
+    def agregar_partida_terminada(self, partida):
+        self.partidas.append(partida)
 
     def continuar_partida(self):
         return messagebox.askyesno(message="¿Desea jugar otra partida?", title="Memotest")
 
     def obtener_ganador_partida(self):
-        resumen = Juego().resumen
-        ganador = resumen[0]
-        max_cantidad_aciertos = resumen[0]['aciertos']
-        misma_cantidad_aciertos = [jugador for jugador in resumen if jugador['aciertos'] == max_cantidad_aciertos]
+        ganador = self.resumen[0]
+        max_cantidad_aciertos = self.resumen[0]['aciertos']
+        misma_cantidad_aciertos = [jugador for jugador in self.resumen if jugador['aciertos'] == max_cantidad_aciertos]
 
         if len(misma_cantidad_aciertos) > 1:
             ganador = sorted(misma_cantidad_aciertos, key=lambda x: x['aciertos'])[0]
@@ -49,7 +53,7 @@ class Partida:
         self.raiz.title("TP2 - Memotest - Ranking")
         ganador = self.obtener_ganador_partida()
         valores_tabla = [['Jugadores', 'Cantidad de aciertos', 'Total de intentos', 'Promedio de intentos']]
-        ultima_partida = Juego().obtener_ultima_partidas()
+        ultima_partida = self.partidas[-1]
         for jugador in ultima_partida:
             valores_fila = [jugador]
             for campo in list(ultima_partida[jugador].keys()):
